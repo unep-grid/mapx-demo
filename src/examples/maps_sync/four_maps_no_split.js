b2c296d813e01f88291a195366cf5904c4f13384
fxi <f@fxi.io>
2023-08-21 15:13:08 +0200

    Removed 3d/terrain for 4 maps no split + update zoom, pitch, bearing, center

--- src/examples/maps_sync/four_maps_no_split.js
+++ src/examples/maps_sync/four_maps_no_split.js
@@ -3,7 +3,7 @@ import { color_dark as style } from "./mapbox_style.js";
 import { generateItems } from "./four_maps_items.js";
 const synchronizerConfig = {
   disableUnify: true,
-  disableTerrain: false,
+  disableTerrain: true,
   disableSplit: true,
   disableLatLongTicks: true,
   ids: ["a", "b", "c", "d"],
@@ -12,12 +12,12 @@ const synchronizerConfig = {
     token:
       "pk.eyJ1IjoiaGVsc2lua2kiLCJhIjoiY2puZW5rZ3N6MGRzYzNwb3drOW12MWEzdyJ9.IZC03hW3hKtBcbMgD0_KPw",
     center: {
-      lng: 28.190820114633624,
-      lat: -3.1254769707673375,
+      lng: 28.17943490684317,
+      lat: -3.1312019115938767,
     },
-    zoom: 13.87,
-    pitch: 73.7867,
-    bearing: 167.383,
+    zoom: 12,
+    pitch: 0,
+    bearing: 90,
     projection: "globe",
     style: style,
   },
